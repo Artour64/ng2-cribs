00244ccbe074cacf3d6545bda7883f8080f11251
Artour Konstantinov <artoukons@gmail.com>
2020-05-08 19:14:50 -0400

    ghjk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
